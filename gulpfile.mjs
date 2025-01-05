76436ab2e991d4705063fc7b33f8fee6695411d4
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-01-05 01:11:49 +0000

    feat: add JSON generation from CSS classes and integrate into build process

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,12 +1,14 @@
 import gulp from "gulp";
-const { series, src, dest } = gulp;
-import clean from "gulp-clean-css";
-import rename from "gulp-rename";
 import dartSass from "sass";
 import gulpSass from "gulp-sass";
-const sass = gulpSass(dartSass);
+import rename from "gulp-rename";
+import clean from "gulp-clean-css";
 import debug from "gulp-debug";
 import { config } from "./yummacss.config.js";
+import { jsonFile } from "./apifile.js";
+
+const { series, src, dest } = gulp;
+const sass = gulpSass(dartSass);
 
 const { capabilities } = config;
 
@@ -16,7 +18,7 @@ function standardFile() {
     : "src/coreless.scss";
 
   return src(entryPoint)
-    .pipe(debug({ title: "Processing File:" }))
+    .pipe(debug({ title: "Processing file:" }))
     .pipe(sass().on("error", sass.logError))
     .pipe(rename("yumma.css"))
     .pipe(dest("dist"));
@@ -24,10 +26,10 @@ function standardFile() {
 
 function minifiedFile() {
   return src("dist/yumma.css", { allowEmpty: true })
-    .pipe(debug({ title: "Minifying File:" }))
+    .pipe(debug({ title: "Minifying file:" }))
     .pipe(clean())
     .pipe(rename({ suffix: ".min" }))
     .pipe(dest("dist"));
 }
 
-export const build = series(standardFile, minifiedFile);
+export const build = series(standardFile, minifiedFile, jsonFile);
