46e67931e56f5b658484067339ee54e72443c276
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-01-29 18:53:17 +0000

    refactor: migrate Sass map functions and migrate to pnpm

--- cli/build.js
+++ cli/build.js
@@ -1,5 +1,5 @@
 import fs from "fs";
-import sass from "sass";
+import * as sass from "sass";
 import CleanCSS from "clean-css";
 
 export async function buildCSS(config, scssEntryPoint, outputPath) {
